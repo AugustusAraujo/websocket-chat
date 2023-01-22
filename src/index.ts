77fa0e62aaf7ff49085aa215ae934afb9c043bc9
Augustus Araújo <71799636+AugustusAraujo@users.noreply.github.com>
2023-01-22 19:40:09 -0300

    feat: mensagens identificadas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
-
-const express = require("express");
-const http = require("http");
+import express from "express";
+import http from "http";
 const socketIO = require("socket.io");
-
 import { Socket } from "socket.io";
 const app = express();
 const server = http.createServer(app);
@@ -13,12 +11,12 @@ io.on("connection", (socket: Socket) => {
   console.log("user connected", socket.id);
   socket.emit("status", "conectado");
   socket.on("SEND_MESSAGE", (data) => {
-    socket.emit("NEW_MESSAGE", data)
-  })
+    io.emit("NEW_MESSAGE", { user: socket.id, message: data });
+  });
 });
 
 app.get("/", (request: Request, response: Response) => {
-  response.sendFile(__dirname + "/vue/index.html");
+  response.sendFile(__dirname + "/vue/chat.html");
 });
 
 app.get("/login", (request: Request, response: Response) => {
